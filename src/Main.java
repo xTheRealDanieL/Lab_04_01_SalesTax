425acf9e23eab7cb40ec59529a90e6003f023a42
Long <nguye3lg@mail.uc.edu>
2022-09-18 22:25:32 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double taxPrice=0;
+        double cost=76584865;
+
+        taxPrice = cost * 5/100;
+        System.out.println("The price is: " + cost);
+        System.out.println("Sales tax is: " + taxPrice);
+
+    }
+}
